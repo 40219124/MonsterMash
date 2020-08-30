5f28ce6b4abacbf503df3ff71cbb23df561c5afd
louie jones-strong Laptop <louiej-s@hotmail.co.uk>
2020-08-30 18:59:37 +0100

    show health delta

--- MonsterMash/Assets/Scripts/BattleLogic/DebugBattleStarter.cs
+++ MonsterMash/Assets/Scripts/BattleLogic/DebugBattleStarter.cs
@@ -3,11 +3,27 @@ using System;
 
 public class DebugBattleStarter : MonoBehaviour
 {
-	public Agent Player;
-	public Agent Enemy;
+
+	[SerializeField] Agent Player;
+	[SerializeField] Agent Enemy;
+
+	[SerializeField] bool WillLoopBattles;
 
 	void Start()
 	{
 		BattleController.Instance.SetupBattle(Player, Enemy);
+	}
+
+	void Update()
+	{
+		if (WillLoopBattles)
+		{
+			var battleState = BattleController.Instance.BattleState;
+			if (battleState == BattleController.eBattleState.PlayerWon ||
+				battleState == BattleController.eBattleState.EnemyWon)
+			{
+				BattleController.Instance.SetupBattle(Player, Enemy);
+			}
+		}
 	}
 }
--- MonsterMash/Assets/Scripts/Body/BodyPart.cs
+++ MonsterMash/Assets/Scripts/Body/BodyPart.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class BodyPart : MonoBehaviour
 {
+	[Header("BattleStats")]
 	public eBodyPartType PartType;
 	public int Armour;
 	public int MaxHealth;
 	public int CurrentHealth { get; private set;}
 	public bool IsAlive { get {return CurrentHealth > 0;}}
+
+	[Space]
+	[Header("UI")]
+	[SerializeField] Text HealthDeltaText;
+	[SerializeField] Animator PartAnimator;
 
 	public enum eBodyPartType
 	{
@@ -30,8 +37,17 @@ public class BodyPart : MonoBehaviour
 		healthDelta = Math.Max(healthDelta, 0);
 
 		CurrentHealth -= healthDelta;
-
 		Debug.Log($"ApplyAttack({damage}) health: {preHealth} -> {CurrentHealth}");
+
+		//now trigger the UI
+		if (HealthDeltaText == null ||
+			PartAnimator == null)
+		{
+			Debug.LogWarning("not all Ui is set up for body part", this);
+			return;
+		}
+		HealthDeltaText.text = $"-{healthDelta}";
+		PartAnimator.SetTrigger("ShowHealthDelta");
 	}
 
 	public override string ToString()
