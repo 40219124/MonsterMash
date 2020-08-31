26436d38e2636ffe7994a7dcf00a20c04c7648d6
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-08-31 18:08:27 +0100

    setup bar again

--- MonsterMash/Assets/Scripts/BattleLogic/BattleUi/TimerUi.cs
+++ MonsterMash/Assets/Scripts/BattleLogic/BattleUi/TimerUi.cs
@@ -5,23 +5,33 @@ using System;
 
 public class TimerUi : MonoBehaviour
 {
-	// [SerializeField] Slider ActionTimeSlider;
-	// [SerializeField] Slider TimeUsedSlider;
+	[SerializeField] Transform ActionTimeSlider;
+	[SerializeField] Transform TimeUsedSlider;
 
-	// void Awake()
-	// {
+	[SerializeField] Transform BarPerant;
+
+	void Awake()
+	{
 		
-	// }
+	}
 
-	// void Update()
-	// {
-	// 	float maxTime = Settings.TurnTime;
-	// 	float timeLeft = BattleController.Instance.TurnTimeLeft;
+	void Update()
+	{
+		float maxTime = Settings.TurnTime;
+		float timeLeft = BattleController.Instance.TurnTimeLeft;
 
-	// 	float timeUsed = maxTime-timeLeft;
-	// 	TimeUsedSlider.value = timeUsed/maxTime;
+		float timeUsed = maxTime-timeLeft;
 
-	// 	float actionTime = timeUsed + BattleController.Instance.TimeLeftOfAction;
-	// 	ActionTimeSlider.value = actionTime/maxTime;
-	// }
+		float actionTime = timeUsed + BattleController.Instance.TimeLeftOfAction;
+
+		if (TimeUsedSlider != null)
+		{
+			TimeUsedSlider.localScale = new Vector3(timeUsed/maxTime, 1, 1);
+		}
+
+		if (ActionTimeSlider != null)
+		{
+			ActionTimeSlider.localScale = new Vector3(actionTime/maxTime, 1, 1);
+		}
+	}
 }
