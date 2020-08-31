485b8e4988e52ed654926fabd05ae87cea2be210
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-09-01 00:07:43 +0100

    Setup timer vfx

--- MonsterMash/Assets/Scripts/BattleLogic/BattleUi/TimerUi.cs
+++ MonsterMash/Assets/Scripts/BattleLogic/BattleUi/TimerUi.cs
@@ -7,8 +7,12 @@ public class TimerUi : MonoBehaviour
 {
 	[SerializeField] Transform ActionTimeSlider;
 	[SerializeField] Transform TimeUsedSlider;
+	[SerializeField] Transform BubbleVfx;
 
 	[SerializeField] Transform BarPerant;
+
+	[SerializeField] float StartPos;
+	[SerializeField] float EndPos;
 
 	void Update()
 	{
@@ -21,16 +25,30 @@ public class TimerUi : MonoBehaviour
 
 		float actionTime = timeUsed + battleController.TimeLeftOfAction;
 
-		BarPerant.localScale = new Vector3(battleController.BattleState == BattleController.eBattleState.PlayerTurn? 1 : -1, 1, 1);
-
-		if (TimeUsedSlider != null)
+		float vfxProgress = timeUsed/maxTime;
+		if (battleController.BattleState == BattleController.eBattleState.PlayerTurn)
 		{
-			TimeUsedSlider.localScale = new Vector3(timeUsed/maxTime, 1, 1);
+			BarPerant.localScale = new Vector3(1, 1, 1);
+
+			TimeUsedSlider.position = new Vector3(StartPos, TimeUsedSlider.position.y, 0);
+			ActionTimeSlider.position = new Vector3(StartPos, ActionTimeSlider.position.y, 0);
+		}
+		else
+		{
+			BarPerant.localScale = new Vector3(-1, 1, 1);
+			TimeUsedSlider.position = new Vector3(EndPos, TimeUsedSlider.position.y, 0);
+			ActionTimeSlider.position = new Vector3(EndPos, ActionTimeSlider.position.y, 0);
+			vfxProgress = 1-vfxProgress;
 		}
 
-		if (ActionTimeSlider != null)
+		TimeUsedSlider.localScale = new Vector3(timeUsed/maxTime, 1, 1);
+		ActionTimeSlider.localScale = new Vector3(actionTime/maxTime, 1, 1);
+
+
+		if (BubbleVfx != null)
 		{
-			ActionTimeSlider.localScale = new Vector3(actionTime/maxTime, 1, 1);
+			vfxProgress = StartPos + vfxProgress*(EndPos-StartPos);
+			BubbleVfx.position = new Vector3(vfxProgress, BubbleVfx.position.y, BubbleVfx.position.z);
 		}
 	}
 }
