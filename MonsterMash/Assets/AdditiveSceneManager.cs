0d128ce417a17a05a633808f5db0af30953a9d0a
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2020-09-02 00:02:03 +0100

    Manager scripts
    Scripts to support additive loading of scenes.

---
+++ MonsterMash/Assets/AdditiveSceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdditiveSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    Camera localCamera;
+    [SerializeField]
+    Canvas localRenderTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(FindObjectOfType<MainManager>() != null)
+        {
+            foreach(Camera c in FindObjectsOfType<Camera>())
+            {
+                if (c.name == localCamera.name)
+                {
+                    c.transform.position = localCamera.transform.position;
+                    break;
+                }
+            }
+            localCamera.gameObject.SetActive(false);
+            localRenderTexture.gameObject.SetActive(false);
+
+        }
+    }
+}
