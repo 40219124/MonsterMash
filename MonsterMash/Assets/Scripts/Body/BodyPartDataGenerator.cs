1ae8f257a1c32da8ecb0113d28f9cf7ea8a24140
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2020-09-02 05:09:30 +0100

    Progress to easy body part data
    It doesn't yet

---
+++ MonsterMash/Assets/Scripts/Body/BodyPartDataGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBodyPart", menuName = "ScriptableObjects/BodyPartData")]
+public class BodyPartDataGenerator : ScriptableObject
+{
+    public BodyPart.eBodyPartSlotType BodyPartType;
+    public EMonsterType MonsterType;
+
+    public int HealthAvg;
+    public int HealthVariation;
+    public int ArmourAvg;
+    public int ArmourVariation;
+    public int DamageAvg;
+    public int DamageVariation;
+    public int AttackTime;
+
+    public BodyPartData GenerateBodyPartData()
+    {
+        return new BodyPartData(
+            BodyPartType,
+            MonsterType,
+            HealthAvg + Random.Range(-HealthVariation, HealthVariation + 1),
+            ArmourAvg + Random.Range(-ArmourVariation, ArmourVariation + 1),
+            DamageAvg + Random.Range(-DamageVariation, DamageVariation + 1),
+            AttackTime);
+    }
+}
