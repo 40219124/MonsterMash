0c77a69a1960d67caea22b6cc5e0699bd7c4135e
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-09-03 00:24:21 +0100

    hook up bigger numbers for damge

--- MonsterMash/Assets/Scripts/Body/BodyPart.cs
+++ MonsterMash/Assets/Scripts/Body/BodyPart.cs
@@ -32,6 +32,20 @@ public class BodyPart : MonoBehaviour
         Torso,
     }
 
+	void Awake()
+	{
+		if (BodyPartImage == null ||
+			HealthDeltaNumber == null ||
+			StatBox == null ||
+            PartAnimator == null)
+        {
+            Debug.LogWarning("not all Ui is set up for body part", this);
+            return;
+        }
+
+		HealthDeltaNumber.UseLargeNumbers = true;
+	}
+
     public void ApplyAttack(int damage)
     {
         int preHealth = CurrentHealth;
@@ -43,14 +57,8 @@ public class BodyPart : MonoBehaviour
 		CurrentHealth = Math.Max(CurrentHealth, 0);
         Debug.Log($"ApplyAttack({damage}) health: {preHealth} -> {CurrentHealth}");
 
-        //now trigger the UI
-        if (HealthDeltaNumber == null ||
-            PartAnimator == null)
-        {
-            Debug.LogWarning("not all Ui is set up for body part", this);
-            return;
-        }
         HealthDeltaNumber.SetNumber(healthDelta);
+
         PartAnimator.SetTrigger("ShowHealthDelta");
 		PartAnimator.SetTrigger("Hit");
 		PartAnimator.SetBool("Dead", !IsAlive);
