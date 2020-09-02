4430f42f9af57bb47862cba78d498e3df5756224
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-09-02 22:14:06 +0100

    ai fix

--- MonsterMash/Assets/Scripts/BattleLogic/Agent.cs
+++ MonsterMash/Assets/Scripts/BattleLogic/Agent.cs
@@ -49,7 +49,9 @@ public class Agent : MonoBehaviour
 		
 		if (LockedAttacker != Body.eBodyPartType.None &&
 			LockedTarget != Body.eBodyPartType.None &&
-			battleController.TimeLeftOfAction <= 0)
+			battleController.TimeLeftOfAction <= 0 &&
+			(battleController.BattleState == BattleController.eBattleState.PlayerTurn ||
+			battleController.BattleState == BattleController.eBattleState.EnemyTurn))
 		{
 			var action = new Action(Body, LockedAttacker, Opponent.Body, LockedTarget);
 			BattleController.Instance.TryAction(action);
@@ -104,23 +106,6 @@ public class Agent : MonoBehaviour
 		}
 	}
 
-	public int SortBylimbAscending(Limb limb1, Limb limb2)
-    {
-		var damage1 = limb1.Damage;
-		var damage2 = limb2.Damage;
-		if (damage1 == damage2)
-		{
-			var time1 = limb1.AttackTime;
-			var time2 = limb2.AttackTime;
-			return time1.CompareTo(time2);
-		}
-		else
-		{
-			return damage1.CompareTo(damage2);
-		}
-    }
-
-
 	Body.eBodyPartType AiPickedTarget;
 	float PickAttackerTime = 0;
 	float PickTargetTime = 0;
@@ -132,12 +117,16 @@ public class Agent : MonoBehaviour
 			if (SelectedPart == Body.eBodyPartType.None ||
 				AiPickedTarget == Body.eBodyPartType.None)
 			{
+				Debug.Log($"AI Picked action but one none attacker: \"{SelectedPart}\" target \"{AiPickedTarget}\"");
 				SelectedPart = Body.eBodyPartType.None;
 				AiPickedTarget = Body.eBodyPartType.None;
 			}
-			Debug.Log($"AI Picked action attacker: {SelectedPart} target {AiPickedTarget}");
-			PickAttackerTime = Random.Range(Settings.AiPickAttackerMinTime, Settings.AiPickAttackerMaxTime);
-			PickTargetTime = Random.Range(Settings.AiPickTargetMinTime, Settings.AiPickTargetMaxTime);
+			else
+			{
+				Debug.Log($"AI Picked action attacker: \"{SelectedPart}\" target \"{AiPickedTarget}\"");
+				PickAttackerTime = Random.Range(Settings.AiPickAttackerMinTime, Settings.AiPickAttackerMaxTime);
+				PickTargetTime = Random.Range(Settings.AiPickTargetMinTime, Settings.AiPickTargetMaxTime);
+			}
 		}
 		else
 		{
@@ -161,6 +150,22 @@ public class Agent : MonoBehaviour
 		}
 	}
 
+	public int SortByLimbAscending(Limb limb1, Limb limb2)
+    {
+		var damage1 = limb1.Damage;
+		var damage2 = limb2.Damage;
+		if (damage1 == damage2)
+		{
+			var time1 = limb1.AttackTime;
+			var time2 = limb2.AttackTime;
+			return time1.CompareTo(time2);
+		}
+		else
+		{
+			return damage1.CompareTo(damage2);
+		}
+    }
+
 	Body.eBodyPartType CalBestAction()
 	{
 		var attackLimbList = new List<(Limb, Body.eBodyPartType)>(3);
@@ -169,7 +174,7 @@ public class Agent : MonoBehaviour
 		attackLimbList.Add((Body.LegsPart, Body.eBodyPartType.Leg));
 		
 		//sort my by damage then time
-		attackLimbList.Sort((x,y)=>SortBylimbAscending(x.Item1, y.Item1));
+		attackLimbList.Sort((x,y)=>SortByLimbAscending(x.Item1, y.Item1));
 
 		var bodyPartPrioList = new List<(BodyPart, Body.eBodyPartType)>(4);
 		bodyPartPrioList.Add((Opponent.Body.TorsoPart, Body.eBodyPartType.Torso));
@@ -180,7 +185,9 @@ public class Agent : MonoBehaviour
 		bodyPartPrioList.Add((Opponent.Body.LegsPart, Body.eBodyPartType.Leg));
 
 		var pickedAttacker = Body.eBodyPartType.None;
-		
+		AiPickedTarget = Body.eBodyPartType.None;
+		int bestSecondsToKill = int.MaxValue;
+
 		foreach (var tBodyPart in bodyPartPrioList)
 		{
 			var bodyPart = tBodyPart.Item1;
@@ -191,56 +198,24 @@ public class Agent : MonoBehaviour
 			
 			foreach (var tAttacker in attackLimbList)
 			{
-				if (tAttacker.Item1.Damage - tBodyPart.Item1.Armour >= tBodyPart.Item1.CurrentHealth)
+				if (!tAttacker.Item1.IsAlive ||
+					tAttacker.Item1.AttackTime >= BattleController.Instance.TurnTimeLeft)
+				{
+					continue;
+				}
+
+				int healthDelta = tAttacker.Item1.Damage - tBodyPart.Item1.Armour;
+				if (healthDelta <= 0)
+				{
+					continue;
+				}
+				
+				int secondsToKill = tBodyPart.Item1.CurrentHealth / healthDelta;
+				if (bestSecondsToKill >= secondsToKill)
 				{
 					pickedAttacker = tAttacker.Item2;
 					AiPickedTarget = tBodyPart.Item2;
-				}
-			}
-			if(AiPickedTarget != Body.eBodyPartType.None)
-			{
-				break;
-			}
-		}
-
-		if(AiPickedTarget == Body.eBodyPartType.None)
-		{
-			float lowestHealth = float.MaxValue;
-			int targetArmour = 0;
-			foreach (var tBodyPart in bodyPartPrioList)
-			{
-				var bodyPart = tBodyPart.Item1;
-				if (!bodyPart.IsAlive)
-				{
-					continue;
-				}
-
-				if (tBodyPart.Item1.CurrentHealth < lowestHealth)
-				{
-					AiPickedTarget = tBodyPart.Item2;
-					lowestHealth = tBodyPart.Item1.CurrentHealth;
-					targetArmour = tBodyPart.Item1.Armour;
-				}
-			}
-
-			float bestDps = 0;
-			foreach (var tAttacker in attackLimbList)
-			{
-				if (!tAttacker.Item1.IsAlive)
-				{
-					continue;
-				}
-				if (targetArmour >= tAttacker.Item1.Damage)
-				{
-					continue;
-				}
-
-				var numAttacks = Mathf.Floor(BattleController.Instance.TurnTimeLeft / tAttacker.Item1.AttackTime);
-				var dps = tAttacker.Item1.Damage * numAttacks;
-				if (dps > bestDps)
-				{
-					pickedAttacker = tAttacker.Item2;
-					bestDps = dps;
+					bestSecondsToKill = secondsToKill;
 				}
 			}
 		}
