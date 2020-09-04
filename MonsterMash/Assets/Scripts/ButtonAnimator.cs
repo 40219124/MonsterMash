4d33cafde5e01425bcb2c653bf517683201c8958
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-09-04 21:33:35 +0100

    add button animtors

---
+++ MonsterMash/Assets/Scripts/ButtonAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimator : MonoBehaviour
+{
+	[SerializeField] EInput ButtonType;
+	[SerializeField] Animator Animator;
+
+	void Update()
+	{
+		Animator.SetBool("Pressed", SimpleInput.GetInputActive(ButtonType));
+	}
+
+	public void SetShow(bool show)
+	{
+		gameObject.SetActive(show);
+	}
+
+	public void SetHighlight(bool highlight)
+	{
+		Animator.SetBool("Highlight", highlight);
+	}
+}
