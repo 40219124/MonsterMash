c37f3d8042377a9fb5b17eca2169da58e89d55f2
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-09-05 02:24:52 +0100

    add data to player profile that we want to track

---
+++ MonsterMash/Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerProfile: MonsterProfile
+{
+	public int NumberOfBattlesPlayed;
+	public int NumberOfRoomsCompleted;
+	public int NumberOfBodyPartsSwapped {get 
+		{return NumberOfArmsSwapped + NumberOfLegsSwapped + NumberOfTorsoSwapped;}
+	}
+	public int NumberOfArmsSwapped;
+	public int NumberOfLegsSwapped;
+	public int NumberOfTorsoSwapped;
+	public int NumberOfLimbsLost;
+
+	public int NumberOfHealingPotionsUsed;
+
+	public int TotalDamageDealt;
+	public int TotalDamageReceived;
+	public int TotalIncomingDamageBlocked;
+	public int TotalOutGoingDamageBlocked;
+	public int TotalHealthHealed;
+}
