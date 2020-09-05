e1d665ffa1ab9bb2ec00000d6f5570972bc532e4
Louie Jones-Strong <louie.jonesstrong@t-tales.com>
2020-09-05 01:47:18 +0100

    now check that you don't target dead body parts

--- MonsterMash/Assets/Scripts/BattleLogic/Agent.cs
+++ MonsterMash/Assets/Scripts/BattleLogic/Agent.cs
@@ -73,7 +73,7 @@ public class Agent : MonoBehaviour
 			{
 				case (EInput.dpadUp):
 				{
-					if (LockedAttacker != Body.eBodyPartType.None)
+					if (LockedAttacker != Body.eBodyPartType.None && Body.LeftArmPart.IsAlive)
 					{
 						SelectedPart = Body.eBodyPartType.Torso;
 					}
@@ -81,7 +81,8 @@ public class Agent : MonoBehaviour
 				}
 				case (EInput.dpadLeft):
 				{
-					if (Body.LeftArmPart.IsValidAttacker() || LockedAttacker != Body.eBodyPartType.None)
+					if ((LockedAttacker == Body.eBodyPartType.None && Body.LeftArmPart.IsValidAttacker()) || 
+						(LockedAttacker != Body.eBodyPartType.None && Body.LeftArmPart.IsAlive))
 					{
 						SelectedPart = Body.eBodyPartType.LeftArm;
 					}
@@ -89,7 +90,8 @@ public class Agent : MonoBehaviour
 				}
 				case (EInput.dpadRight):
 				{
-					if (Body.RightArmPart.IsValidAttacker() || LockedAttacker != Body.eBodyPartType.None)
+					if ((LockedAttacker == Body.eBodyPartType.None && Body.RightArmPart.IsValidAttacker()) || 
+						(LockedAttacker != Body.eBodyPartType.None && Body.RightArmPart.IsAlive))
 					{
 						SelectedPart = Body.eBodyPartType.RightArm;
 					}
@@ -97,7 +99,8 @@ public class Agent : MonoBehaviour
 				}
 				case (EInput.dpadDown):
 				{
-					if (Body.LegsPart.IsValidAttacker() || LockedAttacker != Body.eBodyPartType.None)
+					if ((LockedAttacker == Body.eBodyPartType.None && Body.LegsPart.IsValidAttacker()) || 
+						(LockedAttacker != Body.eBodyPartType.None && Body.LegsPart.IsAlive))
 					{
 						SelectedPart = Body.eBodyPartType.Leg;
 					}
